b489f4688f8166c6ffe8f33c10aed30b068b8d72
Milan Jaric <327155+mjaric@users.noreply.github.com>
2025-05-14 13:14:18 +0200

    adding tidewave

--- apps/lora_web/lib/lora_web/endpoint.ex
+++ apps/lora_web/lib/lora_web/endpoint.ex
@@ -25,6 +25,11 @@ defmodule LoraWeb.Endpoint do
     gzip: false,
     only: LoraWeb.static_paths()
 
+  # Load Tidewave MCP server
+  if Code.ensure_loaded?(Tidewave) do
+    plug Tidewave
+  end
+
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do
